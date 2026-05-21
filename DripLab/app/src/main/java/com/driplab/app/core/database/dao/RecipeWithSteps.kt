ab8a84588cecbdc7e300a1bcadcf04505176be91
redtax <redtax@users.noreply.github.com>
2026-05-21 11:46:37 +0000

    fix: 移除RecipeWithSteps中不必要的@Entity导入

--- DripLab/app/src/main/java/com/driplab/app/core/database/dao/RecipeWithSteps.kt
+++ DripLab/app/src/main/java/com/driplab/app/core/database/dao/RecipeWithSteps.kt
@@ -1,7 +1,6 @@
 package com.driplab.app.core.database.dao
 
 import androidx.room.Embedded
-import androidx.room.Entity
 import androidx.room.Relation
 import com.driplab.app.core.database.entity.RecipeEntity
 import com.driplab.app.core.database.entity.RecipeStepEntity
